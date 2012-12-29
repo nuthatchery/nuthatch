06d279c8fd437a529053388002cfbd05929b3317
Anya Helene Bagge <anya@ii.uib.no>
2012-12-29 02:19:30 +0100

    Beginnings of a simple tree visualisation with LaTeX output

--- src/nuthatch/Main.java
+++ src/nuthatch/Main.java
@@ -1,25 +1,30 @@
 package nuthatch;
 
+import nuthatch.engine.Engine;
+import nuthatch.engine.SimpleTransform;
+import nuthatch.engine.Strategy;
 import nuthatch.engine.Transform;
 import nuthatch.engine.impl.BottomupStrategy;
 import nuthatch.engine.impl.InorderStrategy;
 import nuthatch.engine.impl.StrategicEngine;
 import nuthatch.engine.impl.TopdownStrategy;
+import nuthatch.engine.impl.VisitStrategy;
 import nuthatch.tree.DoubleTree;
 import nuthatch.tree.Tree;
 
 public class Main {
 	public static void main(String[] args) {
 		Tree tree = new DoubleTree("+", "", new DoubleTree("5", ""), new DoubleTree("*", "", new DoubleTree("+", "", new DoubleTree("7", ""), new DoubleTree("3", "")), new DoubleTree("4", "")));
-		
+
 		System.out.println(tree);
-		
-		Transform t = new Transform() {
+
+		Transform t = new SimpleTransform() {
 			@Override
 			public Tree apply(Tree tree) {
 				System.out.print(tree.getName() + " ");
 				return null;
-			}};
+			}
+		};
 		TopdownStrategy topDown = new TopdownStrategy(t);
 		System.out.print("TopDown: ");
 		new StrategicEngine(tree, topDown).engage();
@@ -30,7 +35,7 @@ public class Main {
 		new StrategicEngine(tree, bottomup).engage();
 		System.out.println();
 
-		InorderStrategy inorder = new InorderStrategy(new Transform() {
+		InorderStrategy inorder = new InorderStrategy(new SimpleTransform() {
 
 			@Override
 			public Tree apply(Tree tree) {
@@ -38,15 +43,77 @@ public class Main {
 				return null;
 			}},
 			t,
-			new Transform() {
+			new SimpleTransform() {
 
-			@Override
-			public Tree apply(Tree tree) {
-				System.out.print(")");
-				return null;
-			}});
+				@Override
+				public Tree apply(Tree tree) {
+					System.out.print(")");
+					return null;
+				}});
 		System.out.print("InOrder: ");
 		new StrategicEngine(tree, inorder).engage();
 		System.out.println();
-}
+
+
+		Strategy toTikz = new VisitStrategy(
+				new SimpleTransform() {
+					@Override
+					public Tree apply(Tree tree) {
+						System.out.println("node (" + tree.getNodeId() + ") {" + tree.getName() + "} [->]");
+						return null;
+					}
+				},
+				new SimpleTransform() {
+					@Override
+					public Tree apply(Tree tree) {
+						return null;
+					}
+				},
+				new SimpleTransform() {
+					@Override
+					public Tree apply(Tree tree) {
+						System.out.println("child{");
+						return null;
+					}
+				},
+				new SimpleTransform() {
+					@Override
+					public Tree apply(Tree tree) {
+						System.out.println("}");
+						return null;
+					}
+				});
+		System.out.println("\\documentclass{minimal}");
+		System.out.println("\\usepackage[a4paper,margin=1cm,landscape]{geometry}");
+		System.out.println("\\usepackage{tikz}");
+		System.out.println("\\usetikzlibrary{positioning,shadows,arrows}");
+		System.out.println("");
+		System.out.println("\\begin{document}");
+		System.out.println("\\begin{center}");
+		System.out.println("\\begin{tikzpicture}[");
+		System.out.println("    every node/.style={circle, draw=gray, fill=white, circular drop shadow,text centered, anchor=north, text=black},");
+		System.out.println("    level distance=0.5cm, growth parent anchor=south");
+		System.out.println("]");
+		System.out.print("\\");
+		new StrategicEngine(tree, toTikz).engage();
+		System.out.println(";");
+
+		new StrategicEngine(tree, new TopdownStrategy(new Transform() {
+			@Override
+			public Tree apply(Engine e) {
+				if(e.from(Tree.PARENT)) {
+					if(!e.isRoot()) {
+					System.out.println("\\down{" + e.getCurrent().getBranch(e.from()).getNodeId() + "}{" + e.getCurrent().getNodeId() + "}");
+					}
+				}
+				else {
+					System.out.println("\\up{" + e.getCurrent().getBranch(e.from()).getNodeId() + "}{" + e.getCurrent().getNodeId() + "}");
+				}
+				return null;
+			}})).engage();
+
+		System.out.println("\\end{tikzpicture}");
+		System.out.println("\\end{center}");
+		System.out.println("\\end{document}");
+	}
 }
---
+++ src/nuthatch/engine/impl/VisitStrategy.java
@@ -0,0 +1,40 @@
+package nuthatch.engine.impl;
+
+import nuthatch.engine.Engine;
+import nuthatch.engine.Transform;
+
+public class VisitStrategy extends AbstractStrategy {
+	private final Transform pre;
+	private final Transform beforeChild;
+	private final Transform afterChild;
+	private final Transform post;
+
+	public VisitStrategy(Transform pre, Transform post, Transform beforeChild, Transform afterChild) {
+		this.pre = pre;
+		this.post = post;
+		this.beforeChild = beforeChild;
+		this.afterChild = afterChild;
+	}
+
+	@Override
+	public int visit(Engine e) {
+		if(e.isLeaf()) {
+			e.transform(pre);
+			e.transform(post);
+		}
+		else if(e.from(PARENT)) {
+			e.transform(pre);
+			e.transform(beforeChild);
+		}
+		else if(e.from(LAST)) {
+			e.transform(afterChild);
+			e.transform(post);
+		}
+		else {
+			e.transform(afterChild);
+			e.transform(beforeChild);
+		}
+
+		return e.from() + 1;
+	}
+}
