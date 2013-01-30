93cf4fcdf6467e007db8e280b39342a4fe29e094
Anya Helene Bagge <anya@ii.uib.no>
2013-01-30 03:25:08 +0100

    Add documentation

--- src/nuthatch/engine/impl/RegisteredEngine.java
+++ src/nuthatch/engine/impl/RegisteredEngine.java
@@ -4,6 +4,21 @@ import nuthatch.strategy.Strategy;
 import nuthatch.tree.Tree;
 import nuthatch.tree.TreeCursor;
 
+/**
+ * This engine variant adds a few state registers.
+ * 
+ * This class is intended to be subclassed, with the subclass given as the E
+ * parameter.
+ * 
+ * @author anya
+ * 
+ * @param <Value>
+ *            The value type
+ * @param <Type>
+ *            The type type
+ * @param <E>
+ *            The type of the implemented engine
+ */
 public abstract class RegisteredEngine<Value, Type, E extends RegisteredEngine<Value, Type, E>> extends StrategicEngine<Value, Type, E> {
 	protected StringBuilder sReg;
 	protected int xReg, yReg;
@@ -20,6 +35,17 @@ public abstract class RegisteredEngine<Value, Type, E extends RegisteredEngine<V
 	}
 
 
+	/**
+	 * Append to S register.
+	 * 
+	 * If the register is previously unset, it is initalised to an empty string.
+	 * 
+	 * You may assume that the append operation is reasonably fast, e.g. using a
+	 * StringBuilder internally.
+	 * 
+	 * @param s
+	 *            A string to be appended
+	 */
 	public void appendToS(String s) {
 		if(sReg == null) {
 			sReg = new StringBuilder();
@@ -28,51 +54,112 @@ public abstract class RegisteredEngine<Value, Type, E extends RegisteredEngine<V
 	}
 
 
+	/**
+	 * Add to X register.
+	 * 
+	 * @param value
+	 *            The integer value to be added
+	 */
 	public void addToX(int value) {
 		xReg += value;
 	}
 
 
+	/**
+	 * Add to Y register.
+	 * 
+	 * @param value
+	 *            The integer value to be added
+	 */
 	public void addToY(int value) {
 		yReg += value;
 	}
 
 
+	/**
+	 * Get contents of S register
+	 * 
+	 * @return The string contained in the S register
+	 */
 	public String getS() {
 		return sReg.toString();
 	}
 
 
+	/**
+	 * Get contents of X register
+	 * 
+	 * @return The integer value
+	 */
 	public int getX() {
 		return xReg;
 	}
 
 
+	/**
+	 * Get contents of Y register
+	 * 
+	 * @return The integer value
+	 */
 	public int getY() {
 		return yReg;
 	}
 
 
+	/**
+	 * Get contents of Z register
+	 * 
+	 * @return The data value
+	 */
 	public Object getZ() {
 		return zReg;
 	}
 
 
+	/**
+	 * Set the string in the S register
+	 * 
+	 * The string may later be added to using {@link #appendToS(String)}.
+	 * 
+	 * @param s
+	 *            A string
+	 */
 	public void setS(String s) {
 		sReg = new StringBuilder(s);
 	}
 
 
+	/**
+	 * Set contents of X register
+	 * 
+	 * @param value
+	 *            The integer value
+	 */
 	public void setX(int value) {
 		xReg = value;
 	}
 
 
+	/**
+	 * Set contents of Y register
+	 * 
+	 * @param value
+	 *            The integer value
+	 */
 	public void setY(int value) {
 		yReg = value;
 	}
 
 
+	/**
+	 * Set contents of Z register
+	 * 
+	 * Note that only the reference is stored; if the object is changed
+	 * elsewhere, the change will be reflected by {@link #getZ()}.
+	 * 
+	 * @param value
+	 *            The data value
+	 */
 	public void setZ(Object value) {
 		zReg = value;
 	}
--- src/nuthatch/engine/impl/SimpleEngine.java
+++ src/nuthatch/engine/impl/SimpleEngine.java
@@ -4,6 +4,17 @@ import nuthatch.strategy.Strategy;
 import nuthatch.tree.Tree;
 import nuthatch.tree.TreeCursor;
 
+/**
+ * A simple engine with registers.
+ * 
+ * This class is not intended to be subclassed; extend {@link RegisteredEngine}
+ * instead.
+ * 
+ * @author anya
+ * 
+ * @param <Value>
+ * @param <Type>
+ */
 public final class SimpleEngine<Value, Type> extends RegisteredEngine<Value, Type, SimpleEngine<Value, Type>> {
 
 	public SimpleEngine(TreeCursor<Value, Type> cursor, Strategy<SimpleEngine<Value, Type>> strat) {
