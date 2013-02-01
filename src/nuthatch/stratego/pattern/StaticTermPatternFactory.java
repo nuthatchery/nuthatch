23dab2e03920419407ff186553b88d34244e178b
Anya Helene Bagge <anya@ii.uib.no>
2013-02-01 01:02:28 +0100

    Make convenient patterns for static imports

---
+++ src/nuthatch/stratego/pattern/StaticTermPatternFactory.java
@@ -0,0 +1,74 @@
+package nuthatch.stratego.pattern;
+
+import org.spoofax.interpreter.terms.IStrategoTerm;
+
+import nuthatch.pattern.Pattern;
+import nuthatch.pattern.PatternFactory;
+import nuthatch.pattern.StaticPatternFactory;
+
+public class StaticTermPatternFactory extends StaticPatternFactory {
+	public static final TermPatternFactory PF = TermPatternFactory.instance;
+	
+	public static Pattern<IStrategoTerm, Integer> _ = TermPatternFactory.instance.any();
+	private StaticTermPatternFactory() {
+	}
+
+	@SafeVarargs
+	public static final Pattern<IStrategoTerm, Integer> appl(String name, Pattern<IStrategoTerm, Integer>... children) {
+		return TermPatternFactory.instance.appl(name, children);
+	}
+
+	public static Pattern<IStrategoTerm, Integer> integer() {
+		return TermPatternFactory.instance.integer();
+	}
+	
+
+	public static Pattern<IStrategoTerm, Integer> integer(int i) {
+		return TermPatternFactory.instance.integer(i);
+	}
+
+	@SafeVarargs
+	public static final Pattern<IStrategoTerm, Integer> list(Pattern<IStrategoTerm, Integer>... children) {
+		var("foo");
+		return TermPatternFactory.instance.list(children);
+	}
+	
+	/**
+	 * @see PatternFactory#nodeData(java.lang.Object)
+	 */
+	public static Pattern<IStrategoTerm, Integer> nodeData(IStrategoTerm data) {
+		return TermPatternFactory.instance.nodeData(data);
+	}
+
+	public static Pattern<?, ?> nodeName(String name) {
+		return TermPatternFactory.instance.nodeName(name);
+	}
+	/**
+	 * @see PatternFactory#nodeType(java.lang.Object)
+	 */
+	public static Pattern<IStrategoTerm, Integer> nodeType(Integer type) {
+		return TermPatternFactory.instance.nodeType(type);
+	}
+	
+	public static Pattern<IStrategoTerm, Integer> string() {
+		return TermPatternFactory.instance.string();
+	}
+
+	public static Pattern<IStrategoTerm, Integer> string(String string) {
+		return TermPatternFactory.instance.string(string);
+	}
+
+	@SafeVarargs
+	public static final Pattern<IStrategoTerm, Integer> tuple(Pattern<IStrategoTerm, Integer>... children) {
+		return TermPatternFactory.instance.tuple(children);
+	}
+
+
+	/**
+	 * @see PatternFactory#var(java.lang.String)
+	 */
+	public static Pattern<IStrategoTerm, Integer> var(String name) {
+		return TermPatternFactory.instance.var(name);
+	}
+
+}
--- src/nuthatch/stratego/pattern/TermPatternFactory.java
+++ src/nuthatch/stratego/pattern/TermPatternFactory.java
@@ -4,47 +4,46 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 
 import nuthatch.pattern.Pattern;
 import nuthatch.pattern.PatternFactory;
-import nuthatch.pattern.impl.VarPattern;
 
 public class TermPatternFactory extends PatternFactory<IStrategoTerm, Integer> {
-	private static TermPatternFactory instance;
-
-	public static TermPatternFactory getInstance() {
-		if(instance == null) {
-			instance = new TermPatternFactory();
-		}
-		return instance;
+	static final TermPatternFactory instance = new TermPatternFactory();
+	
+	private TermPatternFactory() {
 	}
 
-	public Pattern<IStrategoTerm, Integer> string(String string) {
-		return node(string, IStrategoTerm.STRING, null);
+	@SafeVarargs
+	public final Pattern<IStrategoTerm, Integer> appl(String name, Pattern<IStrategoTerm, Integer>... children) {
+		return nodeWithChildren(name, IStrategoTerm.APPL, null, children);
+	}
+
+	public Pattern<IStrategoTerm, Integer> integer() {
+		return node(null, IStrategoTerm.INT, null);
+	}
+
+	public Pattern<IStrategoTerm, Integer> integer(int i) {
+		return node(String.valueOf(i), IStrategoTerm.INT, null);
+	}
+	
+
+	@SafeVarargs
+	public final Pattern<IStrategoTerm, Integer> list(Pattern<IStrategoTerm, Integer>... children) {
+		return nodeWithChildren(null, IStrategoTerm.LIST, null, children);
 	}
 
 	public Pattern<IStrategoTerm, Integer> string() {
 		return node(null, IStrategoTerm.STRING, null);
 	}
 	
-
-	public Pattern<IStrategoTerm, Integer> integer(int i) {
-		return node(String.valueOf(i), IStrategoTerm.INT, null);
+	public Pattern<IStrategoTerm, Integer> string(String string) {
+		return node(string, IStrategoTerm.STRING, null);
 	}
 
-	public Pattern<IStrategoTerm, Integer> integer() {
-		return node(null, IStrategoTerm.INT, null);
-	}
-	
-	@SafeVarargs
-	public final Pattern<IStrategoTerm, Integer> appl(String name, Pattern<IStrategoTerm, Integer>... children) {
-		return nodeWithChildren(name, IStrategoTerm.APPL, null, children);
-	}
-
-	@SafeVarargs
-	public final Pattern<IStrategoTerm, Integer> list(Pattern<IStrategoTerm, Integer>... children) {
-		return nodeWithChildren(null, IStrategoTerm.LIST, null, children);
-	}
 	@SafeVarargs
 	public final Pattern<IStrategoTerm, Integer> tuple(Pattern<IStrategoTerm, Integer>... children) {
 		return nodeWithChildren(null, IStrategoTerm.TUPLE, null, children);
+	}
+	public static TermPatternFactory getInstance() {
+		return instance;
 	}
 	
 }
