0ecdd2bc69b6219e8c024bb14c1af2e7775ef4bb
Anya Helene Bagge <anya@ii.uib.no>
2014-08-19 11:22:50 +0200

    missing files from previous commit

---
+++ src/nuthatch/examples/exprlang/Let.java
@@ -0,0 +1,16 @@
+package nuthatch.examples.exprlang;
+
+public class Let extends Expr {
+
+	public Let(Var x, Expr e1, Expr e2) {
+		super("Let", Type.INT, new Expr[] { x, e1, e2 });
+
+	}
+
+
+	@Override
+	protected Expr copy() {
+		return new Let((Var) getChild(0), getChild(1), getChild(2));
+	}
+
+}
---
+++ src/nuthatch/examples/exprlang/Var.java
@@ -0,0 +1,58 @@
+package nuthatch.examples.exprlang;
+
+public class Var extends Expr {
+
+	private String name;
+
+
+	public Var(String s) {
+		super(s, Type.INT, new Expr[] {});
+		name = s;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(!super.equals(obj)) {
+			return false;
+		}
+		if(getClass() != obj.getClass()) {
+			return false;
+		}
+		Var other = (Var) obj;
+		if(name == null) {
+			if(other.name != null) {
+				return false;
+			}
+		}
+		else if(!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public String toString() {
+		return String.valueOf(name);
+	}
+
+
+	@Override
+	protected Expr copy() {
+		return new Var(name);
+	}
+
+}
