91bdd66643775dcc644fcc998b973f454a52782a
Anya Helene Bagge <anya@ii.uib.no>
2014-08-21 05:04:37 +0200

    Sync with changes to Nuthatch

--- src/nuthatch/stratego/pattern/StrategoBuildContext.java
+++ src/nuthatch/stratego/pattern/StrategoBuildContext.java
@@ -1,14 +1,14 @@
 package nuthatch.stratego.pattern;
 
 import nuthatch.pattern.BuildContext;
-import nuthatch.tree.TreeCursor;
+import nuthatch.stratego.adapter.STermCursor;
 
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
-public class StrategoBuildContext implements BuildContext<IStrategoTerm, Integer> {
+public class StrategoBuildContext implements BuildContext<IStrategoTerm, Integer, STermCursor> {
 
 	@Override
-	public <T extends TreeCursor<IStrategoTerm, Integer>> T create(String name, Integer type, IStrategoTerm value, T[] children) {
+	public STermCursor create(String name, Integer type, IStrategoTerm value, STermCursor[] children) {
 		throw new UnsupportedOperationException("not yet implemented");
 	}
 
